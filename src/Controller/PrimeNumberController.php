631cf1a1a28ca052f05d7d58ca3147b5135586b9
Moazzam Khan <moazzamk@gmail.com>
2016-10-08 11:15:36 -0500

    Prime number controller now returns json response

--- src/Controller/PrimeNumberController.php
+++ src/Controller/PrimeNumberController.php
@@ -4,26 +4,45 @@ namespace App\Controller;
 
 use App\Action\FindPrimeNumbersAction;
 use App\Exception\BadRequestException;
+use App\View\JsonRenderer;
+use Psr\Http\Message\RequestInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 class PrimeNumberController
 {
 	/** @var \App\Action\FindPrimeNumbersAction */
 	private $findPrimeNumbersAction;
 
+	/** @var  JsonRenderer */
+	private $renderer;
 
-	
-	public function __construct(FindPrimeNumbersAction $findPrimeNumbersAction)
+
+	/**
+	 * PrimeNumberController constructor.
+	 *
+	 * @param FindPrimeNumbersAction  $findPrimeNumbersAction
+	 * @param JsonRenderer            $jsonRenderer
+	 */
+	public function __construct(FindPrimeNumbersAction $findPrimeNumbersAction, JsonRenderer $jsonRenderer)
 	{
 		$this->findPrimeNumbersAction = $findPrimeNumbersAction;
+		$this->renderer = $jsonRenderer;
 	}
 
-	public function get($request, $response, $args)
+	/**
+	 * @param Request   $request
+	 * @param Response  $response
+	 * @param array     $args
+	 *
+	 * @return \Psr\Http\Message\ResponseInterface
+	 * @throws BadRequestException
+	 */
+	public function getList(Request $request , Response $response, $args)
 	{
-		$number = $request->getAttribute('number');
-		if (empty($number)) {
-			throw new BadRequestException('Please provide a number greater than zero');
-		}
-
+		$number = $request->getParam('number');
 		$ret = $this->findPrimeNumbersAction->run($number);
+
+		return $this->renderer->render($response, $ret);
 	}
 }
